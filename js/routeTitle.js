f17c85d033420bb604ed52797bd2de97cbef1f3f
horo <pylay.luka@gmail.com>
2024-01-29 11:59:41 +0800

    Site deployed by GitHub Actions

---
+++ js/routeTitle.js
@@ -0,0 +1,1 @@
+var OriginTitile=document.title;var titleTime;document.addEventListener("visibilitychange",(function(){if(document.hidden){document.title="w(ﾟДﾟ)w 不要走！再看看嘛！";clearTimeout(titleTime)}else{document.title="♪(^∇^*)欢迎回来！"+OriginTitile;titleTime=setTimeout((function(){document.title=OriginTitile}),2e3)}}));
